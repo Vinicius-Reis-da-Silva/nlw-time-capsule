da6d4a5987c148d92b08a66f3cd7120437a72376
Vinicius Reis da Silva <viniciusreissilva808@gmail.com>
2023-05-19 20:52:20 -0300

    starting setup web

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,19 @@
-import fastify from "fastify";
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
-const app = fastify();
+const app = fastify()
+const prisma = new PrismaClient()
 
-app.get('/hello', () => {
-  return 'hello world'
-});
+app.get('/users', async () => {
+  const users = await prisma.user.findMany()
 
-app.listen({
-  port: 3333,
-}).then(() => {
-  console.log("🚀 HTTP server running on http://localhost:3333")
+  return users
 })
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('🚀 HTTP server running on http://localhost:3333')
+  })
